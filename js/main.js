0a2e35b553c875511ede46b1659bcf419f51db75
Jefferson Teles <telesjefferson2@gmail.com>
2019-03-27 19:27:47 -0300

    header, menu mobile sidebar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Mobile Open
+function mobileOpen() {
+  document.querySelector(".navbar-mobile").classList.toggle("show");
+}
+
+// Mobile Close
+function mobileClose() {
+  document.querySelector(".navbar-mobile").classList.remove("show");
+}
